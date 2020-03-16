e9ab993f47c66f601d435079433e2098643d3be5
Mohamad Jaki Andika 80 <jakiandika19@gmail.com>
2020-03-16 09:56:11 +0700

    Javascript Functions

---
+++ src/functions.js
@@ -0,0 +1,37 @@
+// => Function Declaration
+hello("mario");
+function hello(name) {
+    console.log("hello " + name);
+}
+
+
+function multyply(a, b) {
+    return a * b
+}
+
+let result = multyply(3, 2)
+console.log(result)
+
+// => Function Expression
+const hello2 = function hello(name) {
+    console.log("hello " + name);
+}
+hello2("bros");
+
+// => Arrow Function
+const hello3 = name => {
+    console.log("hello " + name);
+};
+hello3("John");
+
+const multyply2 = (a, b = 1) => a * b;
+
+
+console.log(multyply2(4, 5));
+
+
+// => IIFE --> autorun
+(function () {
+    console.log('auto execute');
+}());
+
